406863593c6dd20fed7f88044d9719044a607790
Kkoushik777 <153192029+Kkoushik777@users.noreply.github.com>
2024-05-17 06:58:28 +0000

    adding files to pdlc

---
+++ 3_Implementation/calculator.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2;
+    int result;
+    printf("enter two numbers");
+    scanf("%d %d",&num1,&num2);
+    result = num1+num2;
+    printf("sum of %d and %d is %d\n",num1,num2,result);
+}
